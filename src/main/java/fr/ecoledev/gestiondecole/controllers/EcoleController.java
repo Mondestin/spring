a954e156b5fb75f46ef59991e35c0a6123ff3bf8
Mondestin <sydneymondestin@gmail.com>
2023-04-16 17:50:17 +0200

    feat: add new ecole(s) with classes

--- src/main/java/fr/ecoledev/gestiondecole/controllers/EcoleController.java
+++ src/main/java/fr/ecoledev/gestiondecole/controllers/EcoleController.java
@@ -1,7 +1,12 @@
 package fr.ecoledev.gestiondecole.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +27,12 @@ public class EcoleController {
     return this.erp.findAll();
   }
   
-	
+  @PostMapping("/save")
+  public ResponseEntity<String> saveEcole(@RequestBody List<EcoleEntity> ecoleData) {
+	  
+	erp.saveAll(ecoleData);
+	  
+    return ResponseEntity.ok("Ecole (s) added successfully");
+  }
+
 }
